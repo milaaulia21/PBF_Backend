cb3eb5f9ba345c71541f193dd82f1157a48a078b
Dendi Ardiansyah <dendiardiansyah61@gmail.com>
2025-05-14 23:57:08 +0700

    updateupdate

--- app/Helpers/jwt_helper.php
+++ app/Helpers/jwt_helper.php
@@ -5,8 +5,6 @@ use Firebase\JWT\Key;
 
 function generateJWT($data)
 {
-    log_message('debug', 'ENV JWT_SECRET_KEY: ' . getenv('JWT_SECRET_KEY'));
-    log_message('debug', 'ENV JWT_ISSUERRRR: ' . getenv('JWT_ISSUERRRR'));
 
     $key = getenv('JWT_SECRET_KEY');
     $payload = [
@@ -16,7 +14,6 @@ function generateJWT($data)
         'exp' => time() + 60 * 60 * 24,
     ];
 
-    log_message('debug', 'Payload: ' . print_r($payload, true));
     return JWT::encode($payload, $key, 'HS256');
 }
 
